daacffc158c23826cfb17573e1c421b2b925cb34
Isra123451 <146052190+Isra123451@users.noreply.github.com>
2024-04-01 12:13:10 +0800

    1_04_2024

---
+++ stutur data/meet2.py
@@ -0,0 +1,21 @@
+# belajar variabel
+# assigment
+x= 3
+y= 6
+uang =1000
+
+#panggilan 
+print ("Nilai UAS saya =", x)
+print ("Nilai UTS saya =", y)
+print ("sisa UANG saya =", uang)
+
+# penamaan variabel
+niali_a = 9
+kotak10 = 10
+
+# input variabel kedua
+x = 30
+print ("Nilai UAS saya =", x)
+x = 300
+print ("Niali UAS saya =", x)
+
