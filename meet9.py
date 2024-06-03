f6e4986978e0c1494df5bd8a588fb77dcdd139c4
Isra123451 <146052190+Isra123451@users.noreply.github.com>
2024-06-03 10:52:59 +0700

    senin 03 juni

---
+++ meet9.py
@@ -0,0 +1,14 @@
+# percabangan jika maka
+
+name = input("what is your name : ")
+
+# if kondisi : aksi
+
+# if standar
+if name == "isra" : print("mahasiswa ganteng")
+print("end of program")
+
+#
+if name=="isra":
+    print("mahasiswa ganteng")
+    print("mahasiswa ganteng")
